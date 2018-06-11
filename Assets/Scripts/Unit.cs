209d7760aacddd29fac8fda8e0207d113ed1d9b7
MrZexeed <35038595+MrZexeed@users.noreply.github.com>
2018-06-11 19:19:39 +0200

    Update Unit.cs

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,24 +1,75 @@
-﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
-
-	public int hpMax, currentHP, attack, attackMental,defenseMental, defense, speed, stun;
-	public string class;
-	public int range;
+	public int hpMax, currentHP, attack, attackMental, defense, defenseMental, speed, stun, range;
+	public Character.Characterclass c;
+	public List<Items> Inventory;
 	
 	public List <Ability> abilities;
 	public Ability SpecialAbility;
-
+	
 	public Tile CurrentTile;
 
-	public void MoveTo ( Tile destinationTile )
+	private void Start ()
 	{
-		this.transform.position = destinationTile.transform.position; //TODO: ajouter l'offset pour que l'unit ne soit pas dans le sol.
 		
-		this.CurrentTile = destinationTile;
+	}
+
+	public bool Attack ()
+	{
+		Debug.Log ( "Attack." );
+
+		return true;
+	}
+
+	public bool UseFirstAbility ()
+	{
+		Debug.Log ( "UseFirstAbility." );
+
+		return true;
+	}
+
+	public bool UseSecondAbility ()
+	{
+		Debug.Log ( "UseSecondAbility." );
+
+		return true;
+	}
+
+	public bool UseThirdAbility ()
+	{
+		Debug.Log ( "UseThirdAbility." );
+
+		return true;
+	}
+
+	public bool UseSpecialAbility ()
+	{
+		Debug.Log ( "UseSpecialAbility." );
+
+		return true;
+	}
+	
+	public bool MoveTo ( Tile destinationTile )
+	{
+		try
+		{
+			this.CurrentTile.Unit = null;
+
+			this.transform.position = destinationTile.transform.position + new Vector3 ( 0, destinationTile.vertOffset, 0 );
+
+			this.CurrentTile = destinationTile;
+
+			this.CurrentTile.Unit = this;
+
+			return true;
+		} catch ( Exception )
+		{
+			return false;
+		}
 	}
 
 }
